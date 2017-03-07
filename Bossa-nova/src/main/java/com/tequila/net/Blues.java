18cf3b57f0ecc8007ba2aab3bd88e9f4b2dba1bb
lalozhang <412217377@qq.com>
2017-03-07 15:16:40 +0800

    update

--- Bossa-nova/src/main/java/com/tequila/net/Blues.java
+++ Bossa-nova/src/main/java/com/tequila/net/Blues.java
@@ -1,6 +1,9 @@
 package com.tequila.net;
 
+import android.nfc.NfcEvent;
 import android.os.Handler;
+
+import com.tequila.model.BaseParam;
 
 /**
  * Created by lalo on 2017/3/7.
@@ -8,10 +11,48 @@ import android.os.Handler;
 
 public class Blues {
 
+    private final static FeatureBase[] DEFAULT_FEATURE = new FeatureBase[]{};
+
+
+    public static void startRequest(IServiceMap key, BaseParam param,Handler handler,FeatureBase...featureBases){
+        NetworkParam networkParam = genNetworkParam(key,param,featureBases);
+
+
+    }
+
+    private static NetworkParam genNetworkParam(IServiceMap key, BaseParam param,FeatureBase...featureBases){
+
+        if(featureBases==null||featureBases.length==0){
+            featureBases=DEFAULT_FEATURE;
+        }
+        NetworkParam networkParam = new NetworkParam(key,param);
+
+        for(FeatureBase featureBase:featureBases){
+            if(featureBase == FeatureBase.CANCEL_PRE||
+                    featureBase == FeatureBase.CANCEL_SAME||
+                    featureBase == FeatureBase.INSERT2HEAD||
+                    featureBase == FeatureBase.INSERT_IN_ORDER){
+                networkParam.addType = featureBase.getCode();
+            }else if(featureBase == FeatureBase.MEM_CACHE){
+
+            }
+        }
+        return null;
+
+    }
+
 
 
     public static void cancelWithHandler(Handler handler){
         OkHttpManager.getInstance().cancelWithHandler(handler);
+    }
+
+    public static void cancel(NetworkParam networkParam){
+        OkHttpManager.getInstance().cancel(networkParam);
+    }
+
+    public static void cancelAll(){
+        OkHttpManager.getInstance().cancelAll();
     }
 
 
--- Bossa-nova/src/main/java/com/tequila/net/FeatureBase.java
+++ Bossa-nova/src/main/java/com/tequila/net/FeatureBase.java
@@ -5,6 +5,44 @@ package com.tequila.net;
  */
 
 public enum FeatureBase {
+    INSERT_IN_ORDER{
 
+        @Override
+        public int getCode() {
+            return AddType.NET_IN_ORDER;
+        }
+    },
+    INSERT2HEAD{
+
+        @Override
+        public int getCode() {
+            return AddType.NET_INSERT2HEAD;
+        }
+    },
+    CANCEL_PRE{
+
+        @Override
+        public int getCode() {
+            return AddType.NET_CANCEL_PRE;
+        }
+    },
+
+    CANCEL_SAME{
+        @Override
+        public int getCode() {
+            return AddType.NET_CANCEL_SAME;
+        }
+    },
+    CANCELABLE,
+    DISK_CACHE,
+    MEM_CACHE,
+
+
+    ;
+
+
+    public int getCode(){
+        return -1;
+    }
 
 }
