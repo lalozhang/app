35a5af518fdaea94b499012d115cf5a265ce96c6
lalozhang <412217377@qq.com>
2017-03-08 10:03:03 +0800

    update

--- Bossa-nova/src/main/java/com/tequila/BaseFragment.java
+++ Bossa-nova/src/main/java/com/tequila/BaseFragment.java
@@ -1,18 +1,34 @@
 package com.tequila;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.tequila.callbacks.HandlerCallbacks;
+import com.tequila.callbacks.NetworkListener;
+import com.tequila.net.NetworkParam;
 
 /**
  * Created by admin on 2017/3/7.
  */
 
-public class BaseFragment extends Fragment {
+public class BaseFragment extends Fragment implements NetworkListener {
 
+    protected Handler mHandler ;
+    protected HandlerCallbacks.FragmentCallback fcb;
+    protected Bundle mBundle;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler(fcb = new HandlerCallbacks.FragmentCallback(this, genCallback()));
+
+        mBundle = savedInstanceState == null?new Bundle():savedInstanceState;
+
+    }
 
     @Nullable
     @Override
@@ -28,5 +44,42 @@ public class BaseFragment extends Fragment {
 
     protected View createView() {
         return null;
+    }
+
+    public Handler.Callback genCallback(){
+        return null;
+    }
+
+    @Override
+    public void onNetworkStart(NetworkParam param) {
+
+    }
+
+    @Override
+    public void onNetworkRunning(NetworkParam param) {
+
+    }
+
+    @Override
+    public void onNetworkComplete(NetworkParam param) {
+
+    }
+
+    @Override
+    public void onError(NetworkParam param) {
+
+    }
+
+    @Override
+    public void onCache(NetworkParam param) {
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if(mBundle!=null){
+            outState.putAll(mBundle);
+        }
+        super.onSaveInstanceState(outState);
     }
 }
